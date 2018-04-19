536a8c9127da5c7c0d4685d96355386c5c946041
jedwardnyc <jacobedwardrico@gmail.com>
2018-04-18 21:23:52 -0400

    bug

--- server/auth/local.js
+++ server/auth/local.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const User = require('../db/models/user');
+const User = require('../db/models/User');
 var jwt = require('jsonwebtoken');
 var bcrypt = require('bcryptjs');
 const { secret } = require('./config');
