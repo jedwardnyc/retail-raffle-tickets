023738bddda8e4c60210192317cfef965ed63a08
jedwardnyc <jacobedwardrico@gmail.com>
2018-04-20 22:48:34 -0400

    seed bug

--- seed.js
+++ seed.js
@@ -1,5 +1,4 @@
 const { Advisor, Ticket, User, db }  = require('./server/db/models');
-const router = require('./server/index');
 
 // randomizers for seeding
 const faker = require('faker');
@@ -29,35 +28,19 @@ const randomTicket = () => {
   return Ticket.build({ reason: chance.capitalize(reason), date })
 };
 
-const randomUser = () => {
-  const gender = chance.gender();
-  const name = chance.name({ gender })
-  const username = `${name[0]}.${name[1]}`
-  const password = 'password'
-  const email = `${username}@gmail.com`
-  return User.build({ name, username, password, email })
-};
-
 const advisors = doTimes(numOfAdvisors, randomAdvisor);
 const tickets = doTimes(numOfTickets, randomTicket);
-const users = doTimes(numOfUsers, randomUser);
 
 const seed = () => {
-  return Promise.all(advisors.map(advisor => advisor.save()))
-    .then(() => Promise.all(tickets.map(ticket => ticket.save()
-      .then(advisor => {
-        const randomAdvisor = chance.pickone(advisors);
-        ticket.setAdvisor(randomAdvisor)
+    return Promise.all(advisors.map(advisor => advisor.save()))
+        .then(() => Promise.all(tickets.map(ticket => ticket.save()
+        .then(advisor => {
+          const randomAdvisor = chance.pickone(advisors);
+          ticket.setAdvisor(randomAdvisor)
       })
-    )))
-    // .then(() => Promise.all(users.map(user => user.save()))
-    //   .then(user => login(user))
-    // );
+    ))
+  );
 };
-
-// const login = (credentials) => {
-//   router.post('/auth/local/register', credentials)
-// };
 
 console.log('Syncing...');
 
